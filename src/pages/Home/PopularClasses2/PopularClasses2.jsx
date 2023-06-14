5db513c20ad0c7c53cc7b1981db053aeab092dd7
farjanarahman <mahfuzurrahman.dev@gmail.com>
2023-06-14 17:02:06 +0600

    updated

---
+++ src/pages/Home/PopularClasses2/PopularClasses2.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+
+const PopularClasses2 = () => {
+    const [popularClass, setPopularClass] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/popularClasses')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setPopularClass(data)
+            })
+    }, [])
+    return (
+
+        <div>
+        <h1 className="text-center text-6xl text-blue-400 mt-2">Popular Classes</h1>
+        <hr className="w-1/3 mx-auto mt-4 bg-blue-400" />
+        <div className="grid lg:grid-cols-3 gap-4 mt-4">
+          {popularClass.map(popular => (
+            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+              <figure>
+                <img className="h-64 w-full" src={popular.image} alt="Shoes" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">{popular.name}</h2>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      
+    );
+};
+
+export default PopularClasses2;
---
+++ src/pages/ShowInstructor/ShowInstructor.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import InstructorCard from '../InstructorCard/InstructorCard';
+
+const ShowInstructor = () => {
+    const [instructor,setInstructor] =useState([])
+    useEffect(()=>{
+        fetch('http://localhost:5000/usersrole')
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+            setInstructor(data)
+        })
+    },[])
+    return (
+       <div>
+        <h1 className='text-center text-6xl text-blue-400 mt-2'>Instructor</h1>
+        <hr className='w-1/3 mx-auto mt-4 bg-blue-400'/>
+         <div className='grid lg:grid-cols-3 gap-4 mt-5'>
+            {instructor.map(instruct=><InstructorCard key={instruct._id} instruct={instruct}></InstructorCard>)}
+        </div>
+       </div>
+    );
+};
+
+export default ShowInstructor;
